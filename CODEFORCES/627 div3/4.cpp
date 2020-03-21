4baf291ededd0c85eee46d9719baa21dc3300e8c
Manvi Tyagi <manvityagi770@gmail.com>
2020-03-21 11:29:05 +0530

    cf div3

---
+++ CODEFORCES/627 div3/4.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+#define LL long long
+#define ULL unsigned long long
+#define LB lower_bound
+#define MOD1 1000000007
+#define MOD2 1000000009
+
+void erast()
+{
+    int n;
+    vector<char> is_prime(n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i <= n; i++)
+    {
+        if (is_prime[i] && (long long)i * i <= n)
+        {
+            for (int j = i * i; j <= n; j += i)
+                is_prime[j] = false;
+        }
+    }
+}
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main()
+{
+    static const int _ = []() {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        return 0;
+    }();
+
+    LL n;
+    cin >> n;
+    vector<LL> a(n), b(n), c(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+        c[i] = b[i] - a[i];
+    }
+
+    sort(c.begin(), c.end());
+    LL ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (c[i] <= 0)
+            continue;
+        int pos = lower_bound(c.begin(), c.end(), -c[i] + 1) - c.begin();
+        ans += i - pos;
+    }
+    cout << ans << endl;
+
+    return 0;
+}
