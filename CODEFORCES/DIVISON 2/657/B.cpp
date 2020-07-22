60cc6525d28bc5a4b65745e2c812144ddcd65e75
Manvi Tyagi <manvityagi770@gmail.com>
2020-07-22 19:22:06 +0530

    did B 657 by myself :)

--- CODEFORCES/DIVISON 2/657/B.cpp
+++ CODEFORCES/DIVISON 2/657/B.cpp
@@ -29,7 +29,7 @@ int main()
     {
         LL l, r, m;
         cin >> l >> r >> m;
-        LL a, b, c;
+        LL a = 1, b = 1, c = 1;
 
         for (a = l; a <= r; a++)
         {
@@ -38,30 +38,18 @@ int main()
                 b = l, c = l;
                 break;
             }
+            int quo = m / a;
             int rem = m % a;
+            int add = a * (quo + 1) - m;
+            b = l;
+            c = b + add;
+            if (c <= r)
+                break;
 
-        
-                // int op1 = m + rem;
-                b = l;
-                c = b + rem;
-                //c-b = b+rem-b = rem
-                if (c <= r)
-                {
-                    break;
-                }
-
-            // int op2 = m - rem;
-            if (m - rem >= a)
-            {
-                c = l;
-                b = c + rem;
-                if (b <= r)
-                {
-                    break;
-                }
-            }
-
-            //c-b = c - c - rem
+            b = r;
+            c = b - rem;
+            if (c >= l)
+                break;
         }
 
         cout << a << " " << b << " " << c << "\n";
